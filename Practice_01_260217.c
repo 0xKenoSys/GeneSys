99cf56fda1b46936d22725f8f01065f0fe3a9751
0xKenoSys <294089357@qq.com>
2026-02-17 20:40:44 +0800

    feat(practice): on 260217

---
+++ Practice_01_260217.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;
+    int *p = &a;
+    printf("%d, %d\n", a, *p);  //两种方式都可以输出a的值RR
+    return 0;
+}
+
+
+//
+// Created by Madeleine on 2026/2/17.
+//
