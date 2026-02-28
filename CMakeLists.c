04523d5119ca71f6708e254195d1c0d41fc23960
0xKenoSys <294089357@qq.com>
2026-02-28 19:26:07 +0800

    chore: clean up files in main branch rename the project file

--- CMakeLists.c
+++
@@ -1,7 +0,0 @@
-cmake_minimum_required(VERSION 4.1)
-project(Desktop)
-
-set(CMAKE_CXX_STANDARD 20)
-
-add_executable(Desktop main.cpp)
-sdq1sdfafadf
--- HaveATry.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main_01() {
-    puts("Long Lives Madelina");
-    return 0;
-}
-// Created by Madeleine on 2026/1/15.
-//
