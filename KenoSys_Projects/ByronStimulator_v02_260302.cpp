c0a842e0b8471a17ca0c47ee5bfac9f606583a55
0xKenoSys <294089357@qq.com>
2026-03-02 23:41:10 +0800

    feat(project): delete << between two lines, leaving only space between

--- KenoSys_Projects/ByronStimulator_v02_260302.cpp
+++ KenoSys_Projects/ByronStimulator_v02_260302.cpp
@@ -4,7 +4,8 @@ int main() {
     std::cout << "========================================================================" << std::endl;
     std::cout << "                       Byron Stimulator Ver.01                          " << std::endl;
 
-    std::cout << "\n\"With pleasure drugged, he almost longed for woe,\n" << "And even for change of scene would seek the shades below.\"\n" << std::endl;
+    std::cout << "\n\"With pleasure drugged, he almost longed for woe,\n"
+                 "And even for change of scene would seek the shades below.\"\n" << std::endl;
 
     std::cout << "                             ---Lord Byron, Childe Harold's Pilgrimage\n" << std::endl;
 
