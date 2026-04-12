40de148d32a9d58289425a5e174e6f187d5e7f89
0xKenoSys <294089357@qq.com>
2026-04-12 21:38:21 +0800

    feat: practice malloc and free, and traversing the string with a pointer; have the combination of execution and judgment

--- DailyPractice/Practice_01_260331.c
+++ DailyPractice/Practice_01_260331.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main() {
     //任务要求：编写一个动态字符串处理程序
     char *line_buffer = NULL;   //这个星号是啥意思
     int length;
-    char string;/*不初始化吗？*/
+    //char string;/*不初始化吗？*/
     //输入阶段： 你的程序需要首先在终端提示用户：“请输入你打算存储的字符串的最大长度（例如 50）：”。读取用户输入的这个整数。
-    printf("请输入你打算储存的字符串的最大长度。");
-    scanf("%d",&length);
+    printf("请输入你打算储存的字符串的最大长度（例如50）：");
+    //scanf("%d",&length);
+    //if (length <= 0) {  //习惯：检查输入是否有效。我认为这也是一种边界条件意识
+    if (scanf("%d", &length) != 1){ //检查scanf的返回值，确保用户输入了正确的整数 //scanf("%d", &length)读入几个变量，就变成数字几。比如读入a，就是0
+    printf("长度无效\n");
+        return 1;
+    }
+
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF){ }
     //动态分配： 根据用户输入的长度，使用 malloc 在堆区动态分配一块足够大的字符（char）数组。
-    line_buffer = (char *)malloc(length * sizeof(char));
+    line_buffer = (char *)malloc((length+1) * sizeof(char));    //+1:加一个结束符
+
     //void* malloc(unsigned nbytes) {
         /*先打样，再举一反三*/
         /*把leetcode第一题做了再来*/
@@ -24,29 +34,42 @@ int main() {
 //if return != 0{ /*如何检查返回值呢？*/
 //printf("error");
     //return 1;
-    //    printf("请输入字符串：");
-    scanf("%s", &string);
 //}
     if (line_buffer == NULL) {
         printf("内存不足，分配失败\n");
         return 1;
     }
     //数据写入： 如果分配成功，提示用户：“请输入字符串：”。将用户输入的字符串读入到你刚才动态分配的堆内存中。
+    printf("请输入字符串：");
+    if (fgets(line_buffer, length + 1, stdin) != NULL) {    //fgets会读入\n，要把它踢掉  //如果读入信息，返回line_buffer，当然不等于NULL；若读到一场空，就不必剔除\n。原子感的执行和判断一体化：去做这件事，如果没有预期回馈，立即停止。
+        line_buffer[strcspn(line_buffer, "\n")] = '\0'; //strcspn: string span complement 把\n替换为\0
+    }
+    //scanf("%s", line_buffer);   //line_buffer是一个地址，直接写入
 //if (p->s.size >= nunits) {  /*如何表示分配成功？我的理解是指针指到的一块size足够大。我不知道整个分配过程如何引入指针，如何用*/
-
 //}
-    sprintf(line_buffer, "%s", &string);
+    //sprintf(line_buffer, "%s", &string);
     //数据处理： 遍历这个动态分配的字符数组，把里面的所有小写字母全部转换成大写字母（证明你确实掌握了对这块堆内存的读写控制权）。
 /*遍历需要指针吧，还要循环*/
-    for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
-        char n = n + 32; /*大小写转换即编码加一个特定值*/
+    int i = 0;
+    while (line_buffer[i] != '\0') {
+        if (line_buffer[i] >= 'a' && line_buffer[i] <= 'z') {
+            line_buffer[i] = line_buffer[i] -32;
+        }
+        i++;
     }
+    //for (line_buffer[i]; i++; )
+    //for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
+        //char n = n + 32; /*大小写转换即编码加一个特定值*/
+
     //输出结果： 打印出转换后的大写字符串。
-printf("%s",&string);    /*指针解引用*/   /*如何输出这么多个字母组成的字符串*/
+    printf("转换后的字符串为：%s\n", line_buffer);
+//printf("%s",&string);    /*指针解引用*/   /*如何输出这么多个字母组成的字符串*/
     //清理现场： 使用 free 释放这块堆内存。
-free((void*)(p+1));
+    free(line_buffer);  //光围绕line_buffer，就这个指针指向了一块分配的内存
+    line_buffer = NULL;
+//free((void*)(p+1));
     //安全收尾： 将指向该内存的指针置为 NULL，彻底切断联系。
-    return int NULL;
+    //return int NULL;
 
     }
 //
