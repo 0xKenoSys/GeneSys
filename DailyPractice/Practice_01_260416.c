6b9a8c415ac43137ebc2740caf78e5fe157e9d7b
0xKenoSys <294089357@qq.com>
2026-04-17 10:01:02 +0800

    docs: add Shell if else statements

--- DailyPractice/Practice_01_260416.c
+++ DailyPractice/Practice_01_260416.c
@@ -2,3 +2,11 @@
 //今天学了Shell，明天或今晚实操一下
 // Created by Madeleine on 2026/4/16.
 //
+
+
+//Shell if else
+
+//if condition  //如果判断条件成立（返回“真”），那么then后面的语句将会被执行。如果不成立，那么不会执行任何语句。从本质上讲，if检测的是命令的退出状态
+//then  //也可以将then和if写在一行：if condition; then
+//  statement(s)
+//fi    //最后必须以fi闭合，fi就是if倒过来拼写。也正是因为有了fi来结尾，所以即使有多条语句也不需要用{}包围起来
