7f7b033d7ddbff8f2ba065b72868cd3061e5f0a4
0xKenoSys <294089357@qq.com>
2026-06-12 14:21:20 +0800

    refactor: rename the variables and update initialization

--- LeetCode_HitTheRoad/Data_Structures_Crash_Course/01_Arrays/01_1470_Shuffle_the_Array.c
+++ LeetCode_HitTheRoad/Data_Structures_Crash_Course/01_Arrays/01_1470_Shuffle_the_Array.c
@@ -6,15 +6,16 @@ int* shuffle(int* nums, int numsSize, int n, int* returnSize){
     //int returnSize = int numsSize; //是这样写吗？
     * returnSize = numsSize;
 
-    int* ans = (int*)malloc(numsSize * sizeof(int));
+    int* result = (int*)malloc(numsSize * sizeof(int));
 
-    int i;
-    for(i = 0; i < n; i++){
-        ans[2 * i] = nums[i];
-        ans[2 * i + 1] = nums[i + n];
+    for(int i = 0; i < n; i++){
+        // 把前半段的元素 x 放进偶数索引
+        result[2 * i] = nums[i];
+        // 把后半段的元素 y 放进奇数索引
+        result[2 * i + 1] = nums[i + n];
     }
 
-    return ans;
+    return result;
 
 
 }
