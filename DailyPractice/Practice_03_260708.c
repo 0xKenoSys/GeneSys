2fe3f796669e380f09b4146a292aab9ac734b4ea
0xKenoSys <294089357@qq.com>
2026-07-08 21:12:26 +1000

    Experimental: implement null pointer access test case

---
+++ DailyPractice/Practice_03_260708.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int *ptr = NULL;    //向地址0x0写入数据
+
+    *ptr = 444;
+
+    printf("你看不到我嘿嘿");
+
+    return 0;
+}
+
+
+//
+// Created by Madeleine on 2026/7/8.
+//
