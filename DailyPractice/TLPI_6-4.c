d695aae775cc7655bd5d1241c928a7a630ffcd54
KenoSys <madeleine.zhang.au@gmail.com>
2026-07-19 18:32:56 +1000

    docs: chapter 6 complete: env API practice, portable clearenv workaround

---
+++ DailyPractice/TLPI_6-4.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include "tlpi_hdr.h"
+
+extern char ** environ;
+
+int main(int argc, char *argv[]) {
+    int j;
+    char **ep;
+
+    //clearenv();
+
+    //environ = NULL;
+
+    while (environ != NULL && *environ != NULL) {
+        char name[128];
+        char *eq = strchr(*environ, '=');
+        snprintf(name, sizeof(name), "%.*s", (int)(eq - *environ), *environ);
+        unsetenv(name);
+    }
+
+    for ( j = 1; j < argc; j++)
+        if (putenv(argv[j]) != 0)
+            errExit("putenv: %s", argv[j]);
+
+    if (setenv("GREET", "Hello world", 0) == -1)
+        errExit("setenv");
+
+    unsetenv("BYE");
+
+    if (environ != NULL)
+    for (ep = environ; *ep != NULL; ep++)
+        puts(*ep);
+
+    exit(EXIT_SUCCESS);
+}
+
+//
+// Created by Yingmei Zhang on 19/7/2026.
+//
